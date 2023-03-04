f20bf9e0c0815dae2e47098bd50e75c3afde696b
KaterynaKucherenko <estorskaya@gmail.com>
2023-03-04 20:41:01 +0300

    done

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,19 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> result = new LinkedList<>();
+        for (String key : projects.keySet()) {
+            if (projects.get(key).contains(developer)) {
+                result.add(key);
+            }
+        }
+        Collections.sort(result, Collections.reverseOrder());
+        Collections.sort(result, Comparator.comparing(String::length).reversed());
+
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,29 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
-    }
+        Map<Integer, Set<String>> result = new HashMap<>();
+        Set<String> temp = new HashSet<>();
+
+       // for (Map.Entry<String, Integer> e : sourceMap.entrySet()){
+            for (String key: sourceMap.keySet()){
+                if (!result.containsKey(key.length())){
+                result.put(key.length(), new HashSet<>());
+                temp = result.get(key.length());
+                temp.add(key);
+                result.put(key.length(), temp);}
+                else {
+                    temp = result.get(key.length());
+                    temp.add(key);
+                    result.put(key.length(), temp);}
+
+
+
+
 }
+        return result; }}
